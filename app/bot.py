bcc3d564781ae4c8d48d0a08ddb514785f6fa1e4
Caio Ferreira <fcaio127@uni9.edu.br>
2022-05-19 20:34:47 -0300

    que trabalho em rs

--- app/bot.py
+++ app/bot.py
@@ -1,10 +1,38 @@
 import requests
 import time
+import json
 
+class TelegramBot:
+    def __init__(self):
+        token = '5255695583:AAGLT4xCXy7MZdLkGWAlRyIC6kwcRk0CBuQ'
+        url_base = f'api.telegram.org/bot{token}/'
+    #inciar o bot
+    def Iniciar(self):
+        update_id = None
+        while True:
+            upgrade = self.obter_mensagens(update_id)
+            mensagens = update ['result']
+            if mensagens:
+                for mensagem in mensagens:
+                    update_id = mensagem['update_id']
+                    chat_id = mensagem['message']['from']['id']
+                    resposta = self.criar_resposta()
+                    self.responder(resposta,chat_id)
+    #receber mensagens
+    def obter_mensagens(self,update_id):
+        link_request = f'{self.url_base}getUpdates?timeout=100'
+        if update_id:
+            link_request = f'{link_request}&offset={update_id + 1}'
+        resultado = requests.get(link_request)
+        return json.loads(resultado.content)
+    #criar respostas
+    def criar_resposta(self):
+        return 'Ola! Seja bem vindo ao Horoscupbot, seu novo jeito de ler seu Horoscup.'
+    #responder
+    def responder(self,resposta,chat_id):
+        #enviar
+        link_envio = f'{self.url_base}sendMessage?chat_id{chat_id}&text={resposta}'
+        request.get(link_envio)
 
-while True:
-    token  = '5255695583:AAGLT4xCXy7MZdLkGWAlRyIC6kwcRk0CBuQ'
-    url_base = f'https://api.telegram.org/bot{token}/getUpdates'
-    resultado = requests.get(url_base)
-    print(resultado.json())
-    time.sleep(10)
+bot = TelegramBot()
+bot.iniciar()
