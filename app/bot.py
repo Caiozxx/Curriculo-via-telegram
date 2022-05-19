b0667e6f7a2d067f6424f728e7443170a7b3b4bd
Caio Ferreira <fcaio127@uni9.edu.br>
2022-05-19 20:09:53 -0300

    Update bot.py

--- app/bot.py
+++ app/bot.py
@@ -4,7 +4,7 @@ import time
 
 while True:
     token  = '5255695583:AAGLT4xCXy7MZdLkGWAlRyIC6kwcRk0CBuQ'
-    url_base ='https://api.telegram.org/bot{token}/getUpdates'
+    url_base = f'https://api.telegram.org/bot{token}/getUpdates'
     resultado = requests.get(url_base)
     print(resultado.json())
     time.sleep(10)
