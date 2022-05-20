b9573db8be903122f31771c1c33f5c70e1afbfff
Caio Ferreira <fcaio127@uni9.edu.br>
2022-05-19 21:03:08 -0300

    surtei ja

--- app/bot.py
+++ app/bot.py
@@ -1,11 +1,12 @@
 import requests
 import time
 import json
+import os
 
 class TelegramBot:
     def __init__(self):
         token = '5255695583:AAGLT4xCXy7MZdLkGWAlRyIC6kwcRk0CBuQ'
-        url_base = f'api.telegram.org/bot{token}/'
+        self.url_base = f'https://api.telegram.org/bot{token}/'
     #inciar o bot
     def Iniciar(self):
         update_id = None
@@ -16,18 +17,21 @@ class TelegramBot:
                 for mensagem in mensagens:
                     update_id = mensagem['update_id']
                     chat_id = mensagem['message']['from']['id']
-                    resposta = self.criar_resposta()
+                    eh_primeira_mensagem = mensagem['message']['message_id'] == 1
+                    resposta = self.criar_resposta(mensagem, eh_primeira_mensagem)
                     self.responder(resposta,chat_id)
     #receber mensagens
     def obter_mensagens(self,update_id):
-        link_requisicao = f'{self.url_base}/getUpdates?timeout=100'
+        link_requisicao = f'{self.url_base}getUpdates?timeout=100'
         if update_id:
             link_requisicao = f'{link_requisicao}&offset={update_id + 1}'
         resultado = requests.get(link_requisicao)
         return json.loads(resultado.content)
     #criar respostas
-    def criar_resposta(self):
-        return 'Ola! Seja bem vindo ao Horoscupbot, seu novo jeito de ler seu Horoscup.'
+    def criar_resposta(self,mensagem,eh_primeira_mensagem):
+        if eh_primeira_mensagem == True:
+            return f'''Ola bem vindo ao nosso HoroscupBot
+        {os.linesep}'''
     #responder
     def responder(self,resposta,chat_id):
         #enviar
